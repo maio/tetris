0b31e19d8ae5aaed47d8fa990a2788046574b393
Marian Schubert <marian.schubert@gmail.com>
2016-03-13 11:06:41 +0100

    Define min Leiningen version in project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/maio/tetris"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.1.2"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
